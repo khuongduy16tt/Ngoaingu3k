1d3a746be0a2518bd0642905d506b5bea23a414f
DoThucFPTYH <dotrithucnknd@gmail.com>
2026-07-11 13:10:37 +0700

    dpl2

--- api/index.js
+++ api/index.js
@@ -1,1 +1,17 @@
-module.exports = require('./[...path].js');
+const handler = require('./[...path].js');
+
+module.exports = function apiIndex(request, response) {
+  const parsedUrl = new URL(request.url, 'http://localhost');
+  const pathQuery = request.query?.path ?? parsedUrl.searchParams.get('path');
+  const path = Array.isArray(pathQuery) ? pathQuery.join('/') : pathQuery;
+
+  if (path && (request.url === '/api' || request.url.startsWith('/api?'))) {
+    const searchParams = parsedUrl.searchParams;
+    searchParams.delete('path');
+
+    const queryString = searchParams.toString();
+    request.url = `/api/${path}${queryString ? `?${queryString}` : ''}`;
+  }
+
+  return handler(request, response);
+};
